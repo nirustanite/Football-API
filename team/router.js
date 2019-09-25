1e2b4e56be1a7f5ca5028a70c1554e226bafb90e
nirustanite <nirustanite@gmail.com>
2019-09-25 14:24:47 +0200

    Added implementation of FindUserById

--- team/router.js
+++ team/router.js
@@ -8,7 +8,7 @@ router.post('/team', (req, res, next) => {
     Team.create(req.body)
         .then(team => res.json(team))
         .catch(next)
-})
+});
 
 router.get('/team',(request,response,next) => {
     Team.findAll()
@@ -16,6 +16,17 @@ router.get('/team',(request,response,next) => {
             if(team){
                 return response.json(team)
             }
+        })
+        .catch(next)
+});
+
+router.get('/team/:id', (req, res, next) => {
+    Team.findByPk(req.params.id)
+        .then(team => {
+            if(team){
+                return res.json(team);
+            }
+            return res.status(404).end();
         })
         .catch(next)
 });
