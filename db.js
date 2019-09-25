ece072146622d25a6a696b111905b2b3a31f2411
nirustanite <nirustanite@gmail.com>
2019-09-25 15:56:25 +0200

    remover force

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:alexander@l
 const db = new Sequelize(databaseUrl);
 
 db
- .sync({force:true})
+ .sync()
  .then(() => console.log('Database schema updated'))
  .catch(console.error)
 
