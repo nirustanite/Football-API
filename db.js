4f80f677a1327b6cd10ead45c8f69f93f2e01da0
nirustanite <nirustanite@gmail.com>
2019-09-25 12:37:23 +0200

    Added Database Connections

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const databaseUrl = process.env.DTABASE_URL || 'postgres://postgres:alexander@localhost:5432/postgres';
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:alexander@localhost:5432/postgres';
 const db = new Sequelize(databaseUrl);
 
 db
